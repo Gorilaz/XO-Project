943d29b97ff3d3881d97796f3780df07914a63b2
Gorilaz <gavrserg89@mail.ru>
2019-06-03 14:06:58 +0300

    Fortget add a file TestField

--- src/io/xo/TestField.java
+++ src/io/xo/TestField.java
@@ -6,6 +6,9 @@ import java.awt.Point;
 
 import org.junit.Test;
 
+import io.xo.exceptions.AlreadyOccupiedException;
+import io.xo.exceptions.InvalidPointException;
+
 public class TestField {
 
 	@Test
@@ -22,10 +25,103 @@ public class TestField {
 	
 	@Test
 	public void testGetSize() throws Exception {
-		Field field = new Field();
+		final Field field = new Field();
 		
 		assertEquals(3, field.getSize());
-
+	}
+	
+	@Test
+	public void testGetFigureWhenFigureIsNotSet() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(0,0);
+		
+		final Figure actualFigure = field.getFigure(inputPoint);
+		
+		assertNull(actualFigure);
+	}
+	
+	@Test
+	public void testGetFigureWhenXIsLessThenZero() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(-1,0);
+		
+		try {
+			final Figure actualFigure = field.getFigure(inputPoint);
+			fail();
+		} catch(final InvalidPointException e) {}
+		
+	}
+	
+	@Test
+	public void testGetFigureWhenYIsLessThenZero() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(0,-1);
+		
+		try {
+			final Figure actualFigure = field.getFigure(inputPoint);
+			fail();
+		} catch(final InvalidPointException e) {}
+		
+	}
+	
+	@Test
+	public void testGetFigureWhenXIsMoreThenFieldSize() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(field.getSize() +1,0);
+		
+		try {
+			final Figure actualFigure = field.getFigure(inputPoint);
+			fail();
+		} catch(final InvalidPointException e) {}
+		
+	}
+	
+	@Test
+	public void testGetFigureWhenYMoreThenFieldSize() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(0,field.getSize());
+		
+		try {
+			final Figure actualFigure = field.getFigure(inputPoint);
+			fail();
+		} catch(final InvalidPointException e) {}
+		
 	}
 
+	@Test
+	public void testGetFigureWhenYXMoreThenFieldSize() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(field.getSize(),field.getSize());
+		
+		try {
+			final Figure actualFigure = field.getFigure(inputPoint);
+			fail();
+		} catch(final InvalidPointException e) {}
+		
+	}
+	
+	@Test
+	public void testGetFigureWhenYXLessThenZero() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(-1,-1);
+		
+		try {
+			final Figure actualFigure = field.getFigure(inputPoint);
+			fail();
+		} catch(final InvalidPointException e) {}
+		
+	}
+	
+	@Test
+	public void test€etFigureWhenYXAlreadyOccupied() throws Exception {
+		final Field field = new Field();
+		final Point inputPoint = new Point(0,0);
+		field.setFigure(inputPoint, Figure.O);
+		
+		try {
+			field.setFigure(inputPoint, Figure.X);	
+			fail();
+		} catch(final AlreadyOccupiedException e) {}
+		
+	}
 }
